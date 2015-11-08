885f038dee890d68cb8d7fa3a9c07ff66b000088
Tomasz Niezgoda <tomek.niezgoda@mail.com>
2015-11-08 11:42:40 +0100

    Changed default Page render.

--- source/scripts/react-components/Page.js
+++ source/scripts/react-components/Page.js
@@ -2,7 +2,7 @@
  * @Author: Tomasz Niezgoda
  * @Date: 2015-11-08 01:35:13
  * @Last Modified by: Tomasz Niezgoda
- * @Last Modified time: 2015-11-08 03:35:46
+ * @Last Modified time: 2015-11-08 10:12:52
  */
 'use strict';
 
@@ -10,7 +10,7 @@ let React = require('react');
 
 let Page = React.createClass({
   render(){
-    return <div>Welcome!</div>;
+    return <h1>Welcome!</h1>;
   }
 });
 
