adda9d906fb6345e927301edb4ae7b92cdddcd74
Tomasz Niezgoda <tomek.niezgoda@mail.com>
2016-01-04 23:02:19 +0100

    Simplified server.js to work with server-creator.

--- source/scripts/server.js
+++ source/scripts/server.js
@@ -2,7 +2,7 @@
  * @Author: Tomasz Niezgoda
  * @Date: 2015-11-07 23:21:37
  * @Last Modified by: Tomasz Niezgoda
- * @Last Modified time: 2015-12-20 19:17:44
+ * @Last Modified time: 2015-12-21 14:57:32
  */
 'use strict';
 
@@ -55,18 +55,45 @@ if(configuration === null){
   );
 }else{
   // let serverLogger;
-  // let express;
-  // let app;
-  // let exphbs;
-  // let assemble;
   // let serverPropsGenerator;
-  let serverCreator = require('server-creator');
+  let express = require('express');
+  let app = express();
+  let assembly = require('react-router-assembly');
+  let control = require('server-creator');
 
-  // listenToShutdownMessageIfChild();
+  let routesElementPath = './routing/routes';
+  let isomorphicLogicPath = './routing/isomorphicLogic';
+
+  function setupRest(){
+    let exphbs;
+
+    app.set('views', __dirname + '/views');
+    exphbs = require('express-handlebars');
+    app.engine('handlebars', exphbs());
+    app.set('view engine', 'handlebars');
+
+    app.use(express.static('public'));
+
+    // this route is not specific to react but useful
+    app.use(function(error, request, response, next) {
+      console.error(error.stack);
+      response.status(500);
+      response.render('server-error.handlebars', {
+        error: error
+      });
+    });
+
+    let server = app.listen(3000, function () {
+      let host = server.address().address;
+      let port = server.address().port;
+
+      console.log('Example app listening at http://%s:%s', host, port);
+
+      control.serverReady(process);
+    });
+  }
 
   // serverLogger = require('morgan');
-  // express = require('express');
-  // app = express();
   // exphbs  = require('express-handlebars');
 
   // // views and templates setup
@@ -77,41 +104,16 @@ if(configuration === null){
   // app.use(serverLogger('dev'));
   // app.use(express.static('public'));
 
-  // assembly = require('react-router-assembly');
+  assembly.attach({
+    app: app,
+    routesElementPath: routesElementPath,
+    serverPropsGeneratorPath: './routing/serverPropsGenerator',
+    isomorphicLogicPath: isomorphicLogicPath,
 
-  // serverPropsGenerator = require('./routing/serverPropsGenerator');
-
-  // assembly.build({
-  //   clientPropsPath: './routing/clientProps',
-  //   routesElementPath: './routing/routes',
-  //   isomorphicLogicPath: './routing/isomorphicLogic',
-  //   extraCompress: process.env.NODE_ENV,
-  //   templatePath: '../views/react-page.handlebars',
-  //   mode: process.env.NODE_ENV === 'production'?assembly.modes.BUILD:assembly.modes.BUILD_AND_WATCH,
-  //   onChange: function(){
-  //     console.log('scripts changed');
-  //   },
-  //   onUpdate: function(attach){
-  //     console.log('scripts updated');
-
-  //     restartServer();
-  //   }
-  // });
-
-  // assemble({
-  //   app: app,
-  //   doneCallback: setupRest.bind(null, app, configuration.port, configuration.host),
-  //   routesElementPath: __dirname + '/routing/routes',
-  //   serverPropsGenerator: serverPropsGenerator,
-  //   templatePath: __dirname + '/../views/react-page.handlebars',
-            //   isomorphicLogicPath: __dirname + '/routing/isomorphicLogic',
-  //   clientPropsPath: __dirname + '/routing/clientProps',
-  //   compressFrontScript: configuration.compressFrontScript,
-  //   additionalTemplateProps: {
-  //     pageTitle: 'got to code'
-  //   }
-  // });
-  // 
- 
-  serverCreator.serverReady(process);
+    onComplete: setupRest//,
+    // templatePath: './views/react-page.handlebars',
+    // additionalTemplateProps: {
+    //   pageTitle: 'got to code'
+    // }
+  });
 }
