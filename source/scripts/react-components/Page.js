e8dffd4f68808df228ddbe32795ea325ac14d11c
Tomasz Niezgoda <tomek.niezgoda@mail.com>
2016-02-28 03:10:26 -0500

    Added information to example website about Live Reload.

--- source/scripts/react-components/Page.js
+++ source/scripts/react-components/Page.js
@@ -2,7 +2,7 @@
  * @Author: Tomasz Niezgoda
  * @Date: 2015-11-08 01:35:13
  * @Last Modified by: Tomasz Niezgoda
- * @Last Modified time: 2016-01-07 02:26:27
+ * @Last Modified time: 2016-02-28 03:08:54
  */
 'use strict';
 
@@ -10,7 +10,12 @@ let React = require('react');
 
 let Page = React.createClass({
   render(){
-    return <h1>It Works!</h1>;
+    return (
+      <div>
+        <h1>It Works!</h1>
+        <p>Use <a target="_blank" href="http://livereload.com/">LiveReload</a> for automatic site reloading during development.</p>
+      </div>
+    );
   }
 });
 
